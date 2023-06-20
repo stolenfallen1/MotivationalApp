0187a4c17542fcf5bfbec01a207ca841a8f3737d
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-20 14:29:16 +0800

    nativewind set-up

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: [],
+  content: ["./App.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
