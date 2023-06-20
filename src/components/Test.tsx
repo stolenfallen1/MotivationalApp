c4b4e844e8c934c0f073977c263fc6b998341865
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-20 14:36:41 +0800

    enable nativewind globally

---
+++ src/components/Test.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Test() {
+  return (
+    <View>
+      <Text className="font-bold text-5xl tracking-tighter">HELLO WORLD</Text>
+    </View>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   content: [],
-  content: ["./App.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
