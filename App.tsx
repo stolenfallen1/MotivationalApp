a701fcf5a58da2ee575b595c7ed0963c03666890
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-20 15:11:11 +0800

    react navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Test from "./src/components/Header";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View className="flex flex-1 justify-center items-center">
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Test} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <Test />
       <StatusBar style="auto" />
     </View>
