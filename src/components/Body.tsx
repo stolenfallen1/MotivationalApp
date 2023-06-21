3ac7ef0b0cbee1a646123d9c2c2bdb13527833a3
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-22 00:30:24 +0800

    xtra btns styling

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Body() {
   return (
-    <View className="mt-5 mb-3 px-40 py-40 bg-gray-200 border-2 border-red-200">
+    <View className="m-5 px-40 py-60 bg-gray-200 border-2 border-red-200">
       <Text>TEST</Text>
     </View>
   );
--- src/components/ExtraBtns.tsx
+++ src/components/ExtraBtns.tsx
@@ -5,15 +5,15 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ExtraBtns() {
   return (
-    <View>
-      <Pressable>
-        <AntDesign name="play" size={24} color="black" />
+    <View className="flex flex-row justify-center items-center">
+      <Pressable className="px-5">
+        <AntDesign name="play" size={50} color="black" />
       </Pressable>
-      <Pressable>
-        <FontAwesome5 name="copy" size={24} color="black" />
+      <Pressable className="px-5">
+        <FontAwesome5 name="copy" size={50} color="black" />
       </Pressable>
-      <Pressable>
-        <MaterialIcons name="save-alt" size={24} color="black" />
+      <Pressable className="px-5">
+        <MaterialIcons name="save-alt" size={50} color="black" />
       </Pressable>
     </View>
   );
