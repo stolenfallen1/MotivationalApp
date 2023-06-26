715f79cfb2f13c99793e8585bddf778698bbbc57
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-26 23:46:24 +0800

    styling quote and author content

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -23,8 +23,8 @@ export default function Body() {
       className="flex justify-center items-center mb-5 bg-gray-200 border-2 border-red-200"
       style={{ width: 350, height: 550 }}
     >
-      <Text>{Quote}</Text>
-      <Text>{Author}</Text>
+      <Text className="text-xl px-5 text-justify mb-10">{Quote}</Text>
+      <Text className="text-lg font-bold italic">- {Author}</Text>
       <View className="absolute bottom-3">
         <Pressable onPress={randomQuote}>
           <Text className="mt-2 px-3 py-2 text-lg bg-red-400">New Quote</Text>
