2b40b91600b5d800547ffc35b56df9d0ef49ccf8
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-26 23:21:10 +0800

    display author from api

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 
 export default function Body() {
   const [Quote, setQuote] = useState();
+  const [Author, setAuthor] = useState();
 
   const randomQuote = () => {
     fetch("https://api.quotable.io/random")
       .then((res) => res.json())
       .then((result) => {
         setQuote(result.content);
+        setAuthor(result.author);
       });
   };
 
@@ -18,6 +20,7 @@ export default function Body() {
       style={{ width: 350, height: 550 }}
     >
       <Text>{Quote}</Text>
+      <Text>{Author}</Text>
       <View className="absolute bottom-3">
         <Pressable onPress={randomQuote}>
           <Text className="mt-2 px-3 py-2 text-lg bg-red-400">New Quote</Text>
