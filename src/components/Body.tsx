05ffade5cdc05a3808050394e13df9c9212c9dd3
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-26 23:19:20 +0800

    dispaly quote from api

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,11 +1,14 @@
 import { Pressable, Text, View } from "react-native";
+import { useState } from "react";
 
 export default function Body() {
+  const [Quote, setQuote] = useState();
+
   const randomQuote = () => {
     fetch("https://api.quotable.io/random")
       .then((res) => res.json())
       .then((result) => {
-        console.log(result);
+        setQuote(result.content);
       });
   };
 
@@ -14,7 +17,7 @@ export default function Body() {
       className="flex justify-center items-center mb-5 bg-gray-200 border-2 border-red-200"
       style={{ width: 350, height: 550 }}
     >
-      <Text>TEST</Text>
+      <Text>{Quote}</Text>
       <View className="absolute bottom-3">
         <Pressable onPress={randomQuote}>
           <Text className="mt-2 px-3 py-2 text-lg bg-red-400">New Quote</Text>
